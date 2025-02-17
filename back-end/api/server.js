8a94dec1c315cc1e9f05268693d9b83827c7268f
Larissa Queiroz <larissaqramos1@gmail.com>
2025-02-17 20:48:42 -0300

    novas alterações

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,3 +1,9 @@
+// API significa Application Programming Interface
+// POST, GET, PUT, DELETE
+// CRUD - Create Read Update Delete
+// Endpoint
+// Middleware
+
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
@@ -9,6 +15,7 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
+// app.use(express.json());
 
 app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
@@ -22,12 +29,10 @@ app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
-//AMBIENTE TESTE CONEXÃO
-app.use(express.static(path.join(__dirname,'../front-end/Spotify-copy/dist')));
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
 
-
-app.get("*", (request, response) => {
-  response.sendFile(__dirname,'../front-end/Spotify-copy/dist/index.html');
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, "../../front-end/Spotify-copy/dist"));
 });
 
 app.listen(PORT, () => {
--- front-end/Spotify-copy/api/api.js
+++ front-end/Spotify-copy/api/api.js
@@ -1,10 +1,14 @@
+// Fetch ou Axios
 import axios from "axios";
 
-//const { NODE_ENV } = process.env;
-const URL = "https://spotifycopy.onrender.com/api";
+// const { NODE_ENV } = process.env;
+// const URL = "http://localhost:3001/api";
+const URL = "https://spotifycopy.onrender.com";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistsArray = responseArtists.data;
 export const songsArray = responseSongs.data;
+
+// console.log(responseArtists.data);
